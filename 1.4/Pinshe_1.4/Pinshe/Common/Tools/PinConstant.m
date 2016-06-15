a33090644dc05787a9a32626761a48d15b4f2509
shiyaorong <vivian@raintr.com>
2016-06-15 11:49:03 +0800

    1.4 上传
    1.4 上传

--- 1.4/Pinshe_1.4/Pinshe/Common/Tools/PinConstant.m
+++ 1.4/Pinshe_1.4/Pinshe/Common/Tools/PinConstant.m
@@ -8,9 +8,6 @@
 
 #import "PinConstant.h"
 
-NSString *const REQUEST_URL = @"http://www.pinshe.org/v1/";
-NSString *const REQUEST_HTML_URL = @"http://www.pinshe.org/html/v1/";
-
 float const fFont11 = 11.0f;
 float const fFont12 = 12.0f;
 float const fFont13 = 13.0f;
